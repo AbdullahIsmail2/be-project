7f4da013bd153097caa7e2f20564e0c54f25af69
Abdullah Ismail <abdullahmusa2003@gmail.com>
2024-07-21 16:05:54 +0100

    ticket-9

--- controllers/GET/getTopics.js
+++ controllers/GET/getTopics.js
@@ -1,7 +1,6 @@
 const getTopicsModel = require("../../models/getTopics");
 
 const getTopicsController = (req, res, next) => {
-	console.log("hi from controller");
 	return getTopicsModel()
 		.then((data) => {
 			console.log(data);
